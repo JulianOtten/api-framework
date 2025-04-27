04dd37d0cdcbc50e1518aad50ed0049805bc9c0f
Julian Otten <juliannottenn@hotmail.com>
2025-04-27 21:16:46 +0200

    clean up index after test logic for queries

--- index.php
+++ index.php
@@ -19,26 +19,4 @@ $builder2 = QueryBuilderFactory::fromConnection();
 
 $start = microtime(true);
 
-$query = $builder->select(
-    'id',
-    'systemname',
-    'price_excl',
-    // $builder2->select('SUM(price_excl)')->from('orders_items oi')->where(eq('oi.product_id', 'p.id'))->as('total_sold'),
-    'another_column'
-)
-->from('products p')
-->join('products_properties_variants ppv', ceq('p.id', 'ppv.product_id'))
-->leftJoin('products_properties_variants_values ppvv', ceq('ppv.id', 'ppvv.product_variant_id'), lt('ppv.id', 10))
-->rightJoin('products_properties_variants ppv', ceq('p.id', 'ppv.product_id'))
-->where(gt('p.id', 10), lt('p.id', 50))
-->and(eq('p.status', 1))
-->orderBy('id', 'DESC')
-->limit(5, 10);
-
-d($query->getBinds());
-
-d($query->build());
-
-dd(microtime(true) - $start);
-
 $app = new App();
