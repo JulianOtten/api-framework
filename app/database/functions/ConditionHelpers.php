78957798b0838ac6bccddd7a2e34b6a99c864eda
Julian Otten <juliannottenn@hotmail.com>
2025-04-30 21:49:17 +0200

    purposefully fail lint check

---
+++ app/database/functions/ConditionHelpers.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Database\QueryBuilder\Functions;
+
+use App\Database\Helpers\Condition;
+
+// function and(Condition ...$condition)
+// {
+
+// }
+
+// function or(Condition ...$condition)
+// {
+
+// }
+
+function eq(string $column, mixed $value): Condition
+{
+    return new Condition($column, '=', $value, true);
+}
+
+function ceq(string $column, string $valueColumn): Condition
+{
+    return new Condition($column, '=', $valueColumn, false);
+}
+
+function lt(string $column, mixed $value): Condition
+{
+    return new Condition($column, '<', $value, true);
+}
+
+function clt(string $column, string $valueColumn): Condition
+{
+    return new Condition($column, '<', $valueColumn, false);
+}
+
+function gt(string $column, mixed $value): Condition
+{
+    return new Condition($column, '>', $value, true);
+}
+
+function cgt(string $column, string $valueColumn): Condition
+{
+    return new Condition($column, '>', $valueColumn, false);
+}
+
+function in(string $column, array $values): Condition
+{
+    return new Condition($column, 'IN', $values, true);
+}
+
+function cin(string $column, string $valueColumn): Condition
+{
+    return new Condition($column, 'IN', $valueColumn, false);
+}
+
+function isNull(string $column): Condition
+{
+    return new Condition($column, 'IS', 'NULL', false);
+}
+
+function isNotNull(string $column): Condition
+{
+    return new Condition($column, 'IS NOT', 'NULL', false);
+}
+
+function lte(string $column, mixed $value): Condition
+{
+    return new Condition($column, '<=', $value, true);
+}
+
+function clte(string $column, string $valueColumn): Condition
+{
+    return new Condition($column, '<=', $valueColumn, false);
+}
+
+function gte(string $column, mixed $value): Condition
+{
+    return new Condition($column, '>=', $value, true);
+}
+
+function cgte(string $column, string $valueColumn): Condition
+{
+    return new Condition($column, '>=', $valueColumn, false);
+}
+
+function notEq(string $column, mixed $value): Condition
+{
+    return new Condition($column, '!=', $value, true);
+}
+
+function cnotEq(string $column, string $valueColumn): Condition
+{
+    return new Condition($column, '!=', $valueColumn, false);
+}
+
+
+
+
+
+
